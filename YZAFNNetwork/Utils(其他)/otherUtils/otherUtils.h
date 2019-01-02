a452658db8b475092c6f5a23e19606b941d5fa0f
jonzhang <jon.zhang@trustlife.com>
2019-01-02 20:45:18 +0800

    otherUtils中增加内容

---
+++ YZAFNNetwork/Utils(其他)/otherUtils/otherUtils.h
@@ -0,0 +1,17 @@
+//
+//  otherUtils.h
+//  YZAFNNetwork
+//
+//  Created by jonzhang on 2019/1/2.
+//  Copyright © 2019年 xinmei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface otherUtils : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YZAFNNetwork/Utils(其他)/otherUtils/otherUtils.m
@@ -0,0 +1,13 @@
+//
+//  otherUtils.m
+//  YZAFNNetwork
+//
+//  Created by jonzhang on 2019/1/2.
+//  Copyright © 2019年 xinmei. All rights reserved.
+//
+
+#import "otherUtils.h"
+
+@implementation otherUtils
+
+@end
